9f5168ed67e3225eeb3b0d9a58e3854208931ad1
leeh697 <leeh697@newschool.edu>
2024-03-29 00:49:18 -0400

    home page

---
+++ exercise5/script.js
@@ -0,0 +1,131 @@
+// #1: TARGET HTML ELEMENTS
+let myElement1 = document.getElementById("circle1-1");
+let myElement2 = document.getElementById("circle1-2");
+let myElement3 = document.getElementById("circle1-3");
+let myElement4 = document.getElementById("circle1-4");
+let myElement5 = document.getElementById("circle1-5");
+
+let myElement6 = document.getElementById("circle2-1");
+let myElement7 = document.getElementById("circle2-2");
+let myElement8 = document.getElementById("circle2-3");
+let myElement9 = document.getElementById("circle2-4");
+let myElement10 = document.getElementById("circle2-5");
+
+let myElement11 = document.getElementById("circle3-1");
+let myElement12 = document.getElementById("circle3-2");
+let myElement13 = document.getElementById("circle3-3");
+let myElement14 = document.getElementById("circle3-4");
+let myElement15 = document.getElementById("circle3-5");
+
+let myElement16 = document.getElementById("circle4-1");
+let myElement17 = document.getElementById("circle4-2");
+let myElement18 = document.getElementById("circle4-3");
+let myElement19 = document.getElementById("circle4-4");
+let myElement20 = document.getElementById("circle4-5");
+
+
+// #2: ARRAY OF COLORS
+let backgroundColors = ["#000000", "#30914a", "#913530", "#305a91"];
+
+// #3: RANDOM COLOR
+let randomBackgroundColor1 = Math.floor(Math.random() * backgroundColors.length);
+let randomBackgroundColor2 = Math.floor(Math.random() * backgroundColors.length);
+let randomBackgroundColor3 = Math.floor(Math.random() * backgroundColors.length);
+let randomBackgroundColor4 = Math.floor(Math.random() * backgroundColors.length);
+let randomBackgroundColor5 = Math.floor(Math.random() * backgroundColors.length);
+
+let randomBackgroundColor6 = Math.floor(Math.random() * backgroundColors.length);
+let randomBackgroundColor7 = Math.floor(Math.random() * backgroundColors.length);
+let randomBackgroundColor8 = Math.floor(Math.random() * backgroundColors.length);
+let randomBackgroundColor9 = Math.floor(Math.random() * backgroundColors.length);
+let randomBackgroundColor10 = Math.floor(Math.random() * backgroundColors.length);
+
+let randomBackgroundColor11 = Math.floor(Math.random() * backgroundColors.length);
+let randomBackgroundColor12 = Math.floor(Math.random() * backgroundColors.length);
+let randomBackgroundColor13 = Math.floor(Math.random() * backgroundColors.length);
+let randomBackgroundColor14 = Math.floor(Math.random() * backgroundColors.length);
+let randomBackgroundColor15 = Math.floor(Math.random() * backgroundColors.length);
+
+let randomBackgroundColor16 = Math.floor(Math.random() * backgroundColors.length);
+let randomBackgroundColor17 = Math.floor(Math.random() * backgroundColors.length);
+let randomBackgroundColor18 = Math.floor(Math.random() * backgroundColors.length);
+let randomBackgroundColor19 = Math.floor(Math.random() * backgroundColors.length);
+let randomBackgroundColor20 = Math.floor(Math.random() * backgroundColors.length);
+
+
+myElement1.style.backgroundColor = backgroundColors[randomBackgroundColor1];
+myElement2.style.backgroundColor = backgroundColors[randomBackgroundColor2];
+myElement3.style.backgroundColor = backgroundColors[randomBackgroundColor3];
+myElement4.style.backgroundColor = backgroundColors[randomBackgroundColor4];
+myElement5.style.backgroundColor = backgroundColors[randomBackgroundColor5];
+
+myElement6.style.backgroundColor = backgroundColors[randomBackgroundColor6];
+myElement7.style.backgroundColor = backgroundColors[randomBackgroundColor7];
+myElement8.style.backgroundColor = backgroundColors[randomBackgroundColor8];
+myElement9.style.backgroundColor = backgroundColors[randomBackgroundColor9];
+myElement10.style.backgroundColor = backgroundColors[randomBackgroundColor10];
+
+myElement11.style.backgroundColor = backgroundColors[randomBackgroundColor11];
+myElement12.style.backgroundColor = backgroundColors[randomBackgroundColor12];
+myElement13.style.backgroundColor = backgroundColors[randomBackgroundColor13];
+myElement14.style.backgroundColor = backgroundColors[randomBackgroundColor14];
+myElement15.style.backgroundColor = backgroundColors[randomBackgroundColor15];
+
+myElement16.style.backgroundColor = backgroundColors[randomBackgroundColor16];
+myElement17.style.backgroundColor = backgroundColors[randomBackgroundColor17];
+myElement18.style.backgroundColor = backgroundColors[randomBackgroundColor18];
+myElement19.style.backgroundColor = backgroundColors[randomBackgroundColor19];
+myElement20.style.backgroundColor = backgroundColors[randomBackgroundColor20];
+
+// #4: BORDER RADIUS SIZE
+let borderRadius=["35%", "40%", "30","25%"]
+
+let randomborderRadius1=Math.floor(Math.random() * borderRadius.length);
+let randomborderRadius2=Math.floor(Math.random() * borderRadius.length);
+let randomborderRadius3=Math.floor(Math.random() * borderRadius.length);
+let randomborderRadius4=Math.floor(Math.random() * borderRadius.length);
+let randomborderRadius5=Math.floor(Math.random() * borderRadius.length);
+
+let randomborderRadius6=Math.floor(Math.random() * borderRadius.length);
+let randomborderRadius7=Math.floor(Math.random() * borderRadius.length);
+let randomborderRadius8=Math.floor(Math.random() * borderRadius.length);
+let randomborderRadius9=Math.floor(Math.random() * borderRadius.length);
+let randomborderRadius10=Math.floor(Math.random() * borderRadius.length);
+
+let randomborderRadius11=Math.floor(Math.random() * borderRadius.length);
+let randomborderRadius12=Math.floor(Math.random() * borderRadius.length);
+let randomborderRadius13=Math.floor(Math.random() * borderRadius.length);
+let randomborderRadius14=Math.floor(Math.random() * borderRadius.length);
+let randomborderRadius15=Math.floor(Math.random() * borderRadius.length);
+
+let randomborderRadius16=Math.floor(Math.random() * borderRadius.length);
+let randomborderRadius17=Math.floor(Math.random() * borderRadius.length);
+let randomborderRadius18=Math.floor(Math.random() * borderRadius.length);
+let randomborderRadius19=Math.floor(Math.random() * borderRadius.length);
+let randomborderRadius20=Math.floor(Math.random() * borderRadius.length);
+
+
+myElement1.style.borderRadius = borderRadius[randomborderRadius1];
+myElement2.style.borderRadius = borderRadius[randomborderRadius2];
+myElement3.style.borderRadius = borderRadius[randomborderRadius3];
+myElement4.style.borderRadius = borderRadius[randomborderRadius4];
+myElement5.style.borderRadius = borderRadius[randomborderRadius5];
+
+myElement6.style.borderRadius = borderRadius[randomborderRadius6];
+myElement7.style.borderRadius = borderRadius[randomborderRadius7];
+myElement8.style.borderRadius = borderRadius[randomborderRadius8];
+myElement9.style.borderRadius = borderRadius[randomborderRadius9];
+myElement10.style.borderRadius = borderRadius[randomborderRadius10];
+
+myElement11.style.borderRadius = borderRadius[randomborderRadius11];
+myElement12.style.borderRadius = borderRadius[randomborderRadius12];
+myElement13.style.borderRadius = borderRadius[randomborderRadius13];
+myElement14.style.borderRadius = borderRadius[randomborderRadius14];
+myElement15.style.borderRadius = borderRadius[randomborderRadius15];
+
+myElement16.style.borderRadius = borderRadius[randomborderRadius16];
+myElement17.style.borderRadius = borderRadius[randomborderRadius17];
+myElement18.style.borderRadius = borderRadius[randomborderRadius18];
+myElement19.style.borderRadius = borderRadius[randomborderRadius19];
+myElement20.style.borderRadius = borderRadius[randomborderRadius20];
+
